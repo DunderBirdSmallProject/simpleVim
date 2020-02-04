414adb471046ad1f7c0b451fc2d18ca77eca590f
DunderBird <522169030@qq.com>
2020-02-04 21:43:52 +0800

    find char

--- src/motion.ts
+++ src/motion.ts
@@ -5,10 +5,6 @@ import { appendFileSync } from 'fs';
 // make sure that when all these functions below
 // was called, the editor exists!
 
-interface Motion {
-    (editor: vscode.TextEditor): vscode.Position;
-};
-
 function currentLine(editor: vscode.TextEditor): vscode.TextLine {
     const cursorPos = editor.selection.active;
     return editor.document.lineAt(cursorPos.line);
@@ -70,4 +66,52 @@ function downChar(editor: vscode.TextEditor): vscode.Position {
     else {
         return cursorPos;
     }
+}
+function downCnt(editor: vscode.TextEditor, cnt: number): vscode.Position {
+    const cursorPos = editor.selection.active;
+    const line = cursorPos.line;
+    let nextLineCnt: number;
+    if(line + cnt < editor.document.lineCount) {
+        nextLineCnt = line + cnt;
+    } else {
+        nextLineCnt = editor.document.lineCount-1;
+    }
+    return new vscode.Position(nextLineCnt, Math.min(lineLength(editor, nextLineCnt), cursorPos.character));
+}
+function upCnt(editor: vscode.TextEditor, cnt: number): vscode.Position {
+    const cursorPos = editor.selection.active;
+    const line = cursorPos.line;
+    let nextLineCnt: number;
+    if(line - cnt >= 0) {
+        nextLineCnt = line - cnt;
+    } else {
+        nextLineCnt = 0;
+    }
+    return new vscode.Position(nextLineCnt, Math.min(lineLength(editor, nextLineCnt), cursorPos.character));
+}
+function nextChar(editor: vscode.TextEditor, c: String): vscode.Position {
+    const cursorPos = editor.selection.active;
+    const line = cursorPos.line;
+    let pChar = cursorPos.character + 1;
+    const text = editor.document.lineAt(line).text;
+    while(pChar < text.length) {
+        if(text[pChar] === c[0]) {
+            return new vscode.Position(line, pChar);
+        }
+        pChar++;
+    }
+    return new vscode.Position(line, text.length);
+}
+function preChar(editor: vscode.TextEditor, c: string): vscode.Position {
+    const cursorPos = editor.selection.active;
+    const line = cursorPos.line;
+    let pChar = cursorPos.character - 1;
+    const text = editor.document.lineAt(line).text;
+    while(pChar >= 0) {
+        if(text[pChar] === c[0]) {
+            return new vscode.Position(line, pChar);
+        }
+        pChar--;
+    }
+    return new vscode.Position(line, 0);
 }
