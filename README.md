82e262de5af332a7e0829f52926acaf7925f5135
DunderBird <522169030@qq.com>
2020-02-10 07:52:17 +0800

    comments

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # simpleVim
+
+一个模仿vim的vscode插件，和原来的vim有一些区别。
+
+感谢：[vscode教程vim例子](https://github.com/Microsoft/vscode-extension-samples/tree/master/vim-sample)
--- src/action.ts
+++ src/action.ts
@@ -28,12 +28,19 @@ type Motion1Dict = {
 type ActionDict = {
     [c: string]: Action;
 };
-
+/**
+ * return a range: (currentPosition, f(currentPosition) )
+ * @param f a function that returns a position based on current position
+ */
 function motionWrapper(f: Pos2Pos): Pos2Range {
     return pos => {
         return new vscode.Range(pos, f(pos));
     };
 }
+/**
+ * return a range: (currentPosition, f(currentPosition) )
+ * @param f a function that and returns a position based on current position and a given character
+ */
 function motionArgWrapper(f: PosArg2Pos): PosArg2Range {
     return (pos, c) => {
         return new vscode.Range(pos, f(pos, c));
@@ -86,6 +93,10 @@ async function enterNewLine(acArg: ActionArg, direct: boolean): Promise<ActionAr
     acArg.editor.revealRange(new vscode.Range(motion.startLine(nextLineEnd), motion.endLine(nextLineEnd)));
     return acArg;
 }
+/**
+ * return an Action that will change the cursor's position
+ * @param motionFunc a motion func that returns a new position
+ */
 function moveCursorWrapper(motionFunc: Pos2Pos) {
     return async (acArg: ActionArg) => {
         const curPos = acArg.editor.selection.active;
@@ -96,6 +107,10 @@ function moveCursorWrapper(motionFunc: Pos2Pos) {
         return acArg;
     };
 }
+/**
+ * return an Action that will change the cursor's position
+ * @param motionFunc a motion func that returns a new position
+ */
 function moveCursorArgWrapper(motionFunc: PosArg2Pos) {
     return async (acArg: ActionArg) => {
         const curPos = acArg.editor.selection.active;
@@ -118,6 +133,9 @@ function opActionWrapper(acFunc: Action): Action {
     };
 }
 
+/**
+ * operation that takes no arguments
+ */
 export let operation0Dict: ActionDict = {
     "i": setInsertMode,
     "a": opActionWrapper(async (acArg: ActionArg) => {
@@ -185,12 +203,18 @@ export let operation0Dict: ActionDict = {
         return acArg;
     },
 };
+/**
+ * operation that takes a range argument
+ */
 export let operation1Dict: ActionDict = {
     "d": opActionWrapper(operation.deleteRange),
     "y": opActionWrapper(operation.copyRange),
     ">": operation.indentRange,
     "<": operation.reIndentRange
 };
+/**
+ * operation that takes a character argument
+ */
 export let operation2Dict: ActionDict = {
     "f": moveCursorArgWrapper(motion.nextCharOnLine),
     "F": moveCursorArgWrapper(motion.previousCharOnLine),
@@ -228,6 +252,9 @@ export let operation2Dict: ActionDict = {
         return acArg;
     }
 };
+/**
+ * virtual commands: that has no concrete meaning itself but represents other commands
+ */
 export let virtualDict: ActionDict = {
     ".": async (acArg: ActionArg) => {
         let parseResult = acArg.v.getLastCmd();
@@ -237,7 +264,10 @@ export let virtualDict: ActionDict = {
         return acArg;
     }
 };
-
+/**
+ * motion function that returns a range
+ * used for interpreter to generate the range argument of an ActionArg
+ */
 export let motion0Dict: Motion0Dict = {
     "w": motionWrapper(motion.nextWordOnLine),
     "b": motionWrapper(motion.lastWordOnLine),
@@ -250,9 +280,13 @@ export let motion0Dict: Motion0Dict = {
     "D": motionWrapper(motion.down20),
     "U": motionWrapper(motion.up20),
 };
-
+/**
+ * motion function that returns a range based on a given character
+ * used for interpreter to generate the range argument of an ActionArg
+ */
 export let motion1Dict: Motion1Dict = {
     "f": motionArgWrapper(motion.nextCharOnLine),
+    "F": motionArgWrapper(motion.previousCharOnLine),
     "i": (pos, c) => {
         const prePos = motion.previousCharOnLine(pos, c);
         const nextPos = motion.nextCharOnLine(pos, c);
